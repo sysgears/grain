54cada58c52ac82bb284042e6bd84e4eb9988b90
Andrey Shevchenko <andrey.shevchenko@sysgears.com>
2013-10-14 11:19:23 +0300

    Simplify build gradle, add Grain launcher.

---
+++ themes/octopress/theme/plugins/com/sysgears/grain/Launcher.groovy
@@ -0,0 +1,16 @@
+package com.sysgears.grain
+
+/**
+ * Grain launcher.
+ */
+class Launcher {
+    
+    /**
+     * Transfers control to Grain.
+     * 
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
+        Main.main(args)
+    }
+}
