b77fc23b69c7d9572c82812830cb2095f64e3760
Andrey Shevchenko <andrey.shevchenko@sysgears.com>
2013-10-17 16:56:50 +0300

    Use java version of Launcher for easier integration with NetBeans IDE.

--- themes/octopress/theme/plugins/com/example/grain/Launcher.java
+++ themes/octopress/theme/plugins/com/example/grain/Launcher.java
@@ -1,18 +1,18 @@
-package com.example.grain
+package com.example.grain;
 
-import com.sysgears.grain.Main
+import com.sysgears.grain.Main;
 
 /**
  * Grain launcher.
  */
-class Launcher {
-    
+public class Launcher {
+
     /**
      * Transfers control to Grain.
      * 
      * @param args command-line arguments
      */
     public static void main(final String[] args) {
-        Main.main(args)
+        Main.main(args);
     }
 }
