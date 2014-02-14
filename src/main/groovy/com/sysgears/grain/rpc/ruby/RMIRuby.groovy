ddff9adc5a0c8bfbfb6d5151db1088f0b1e8ed36
Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>
2014-02-14 17:43:05 +0200

    Use rubygems 2.0.x with ruby 2.0.x, other rubygems versions seems incompatible. Signed-off-by: Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>

--- src/main/groovy/com/sysgears/grain/rpc/ruby/RMIRuby.groovy
+++ src/main/groovy/com/sysgears/grain/rpc/ruby/RMIRuby.groovy
@@ -83,8 +83,10 @@ public class RMIRuby implements Ruby {
             def rubyCmd = rubyFinder.cmd.command
             def ver = rubyFinder.cmd.version
             if (ver.startsWith('ruby 1.')) {
-                // 1.8.11 version is compatible with Ubuntu 12.04 LTS Ruby 1.9.3p0
+                // 1.8.11 version is compatible with Ubuntu 12.04 LTS Ruby 1.9.3
                 rubyGemsDir = installer.install('1.8.11')
+            } else if (ver.startsWith('ruby 2.0.')) {
+                rubyGemsDir = installer.install('2.0.13')
             } else {
                 rubyGemsDir = installer.install()
             }
