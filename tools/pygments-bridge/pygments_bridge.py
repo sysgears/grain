0df930314ce4214d0f77324c5616a4debadb472e
Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>
2016-03-09 14:26:45 +0200

    Set the default encoding for Pygments lexers to UTF-8. Signed-off-by: Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>

--- tools/pygments-bridge/pygments_bridge.py
+++ tools/pygments-bridge/pygments_bridge.py
@@ -2,7 +2,7 @@ import pygments
 from pygments import lexers, formatters, styles, filters
 
 def highlight(code, language):
-    lexer = lexers.get_lexer_by_name(language)
+    lexer = lexers.get_lexer_by_name(language, encoding = 'utf-8')
     formatter = pygments.formatters.get_formatter_by_name('html', outencoding = 'utf-8')    
     res = pygments.highlight(code, lexer, formatter)
     return res
