db9a5c1ccae1bb9f97a0cf41c86f33d5a176a0ea
Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>
2016-03-14 13:30:00 +0200

    Improved error handling for the Pygments highlighting. Signed-off-by: Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>

--- tools/pygments-bridge/pygments_bridge.py
+++ tools/pygments-bridge/pygments_bridge.py
@@ -1,8 +1,15 @@
-import pygments
+import pygments, sys
 from pygments import lexers, formatters, styles, filters
 
 def highlight(code, language):
-    lexer = lexers.get_lexer_by_name(language, encoding = 'utf-8')
-    formatter = pygments.formatters.get_formatter_by_name('html', outencoding = 'utf-8')    
-    res = pygments.highlight(code, lexer, formatter)
-    return res
+    try:
+        lexer = lexers.get_lexer_by_name(language, encoding = 'utf-8')
+        formatter = pygments.formatters.get_formatter_by_name('html', outencoding = 'utf-8')
+        res = pygments.highlight(code, lexer, formatter)
+        return res
+    except:
+        sys.stderr.write("Pygments error: an exception occurred while highlighting the code")
+        sys.stderr.write("\nLanguage: %s"%((language)))
+        sys.stderr.write("\nCode:\n%s\n"%((code)))
+        sys.stderr.flush()
+        raise
